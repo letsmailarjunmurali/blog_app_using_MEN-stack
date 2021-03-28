b38a6a691576bf121bd706711f642a5a07e5540d
arjunm <letsmailarjunmurali@gmail.com>
2021-03-28 11:07:01 +0530

    http_with_different_routing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.end("Hello Node ");
+  if (req.url === "/about") {
+    res.end("The about page");
+  } else if (req.url === "/contact") {
+    res.end("The contact page");
+  } else if (req.url === "/") {
+    res.end("The contact page");
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 server.listen(3000);
