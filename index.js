5b7e3666776a501143184e35dd073c6d78d4764c
arjunm <letsmailarjunmurali@gmail.com>
2021-03-28 13:24:48 +0530

    Serving_static_files_with_express

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static("public"));
+
 app.listen(3000, () => {
   console.log("App listening on port 3000");
 });
 
 app.get("/", (req, res) => {
-  console.log(path.resolve(__dirname, "index.html"));
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
