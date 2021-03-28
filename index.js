a883258981cd11aca52fc2fc85771c2e40ccf363
arjunm <letsmailarjunmurali@gmail.com>
2021-03-28 15:46:42 +0530

    templating_engine_final

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const path = require("path");
 const app = new express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public/pages/index.html"));
+app.get("/index", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "public/pages/index.html"));
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public/pages/about.html"));
+  //res.sendFile(path.resolve(__dirname, "public/pages/about.html"));
+  res.render("about");
 });
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public/pages/contact.html"));
+  //res.sendFile(path.resolve(__dirname, "public/pages/contact.html"));
+  res.render("contact");
 });
 app.get("/post", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public/pages/post.html"));
+  res.render("post");
 });
 app.listen(3000, () => {
   console.log("App listening on port 4000");
