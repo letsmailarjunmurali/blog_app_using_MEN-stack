4841b842bca1ee72bd26f3c7a67c821b00ba59db
arjunm <letsmailarjunmurali@gmail.com>
2021-03-28 12:47:36 +0530

    responding_with_html

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require("http");
+const fs = require("fs");
+const indexpage = fs.readFileSync("index.html");
+const aboutpage = fs.readFileSync("about.html");
+const contactpage = fs.readFileSync("contact.html");
+const notfoundpage = fs.readFileSync("notfound.html");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/about") {
-    res.end("The about page");
+    res.end(aboutpage);
   } else if (req.url === "/contact") {
-    res.end("The contact page");
+    res.end(contactpage);
   } else if (req.url === "/") {
-    res.end("The contact page");
+    res.end(indexpage);
   } else {
     res.writeHead(404);
-    res.end("Page not found");
+    res.end(notfoundpage);
   }
 });
 
