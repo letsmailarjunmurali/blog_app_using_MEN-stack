552ef1bb843f1c5d8696321a2229bee0e1933388
arjunm <letsmailarjunmurali@gmail.com>
2021-03-30 18:52:46 +0530

    request_created

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/index", (req, res) => {
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
   // res.sendFile(path.resolve(__dirname, "public/pages/index.html"));
   res.render("index");
 });
@@ -23,6 +27,14 @@ app.get("/post", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public/pages/post.html"));
   res.render("post");
 });
+
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+app.post("/posts/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 app.listen(3000, () => {
-  console.log("App listening on port 4000");
+  console.log("App listening on port 3000");
 });
